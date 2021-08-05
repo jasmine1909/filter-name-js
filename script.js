a1a902fd8d7b4e735bcec9966d1c09799fee0023
jasmine1909 <jasminedrift@gmail.com>
2021-08-05 19:05:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+filterInput.addEventListener("keyup", filterNames);
+function filterNames() {
+  //get value of input
+  let filterValue = document.getElementById("filterInput").value.toUpperCase();
+  //get names ul
+  let ul = document.getElementById("names");
+  //get li
+  let li = ul.querySelectorAll("li.collection-item");
+  //loop throguh collection item li
+  for (let i = 0; i < li.length; i++) {
+    let a = li[i].getElementsByTagName("a")[0];
+    //if matched
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
